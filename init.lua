4853def37026d0094f84e17977901aad5b2db241
David Walschots <davidwalschots@users.noreply.github.com>
2023-02-28 23:15:24 +0100

    truncate Telescope file search

--- init.lua
+++ init.lua
@@ -258,6 +258,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    path_display = { "truncate" },
     mappings = {
       i = {
         ['<C-u>'] = false,
