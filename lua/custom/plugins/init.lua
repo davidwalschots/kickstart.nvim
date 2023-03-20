b25726b44843e99cb292e436c7abf68cd366d3ce
David Walschots <davidwalschots@users.noreply.github.com>
2023-03-20 23:02:08 +0100

    english, relative line numbers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+-- Set language
+vim.api.nvim_exec('language en_GB', true)
+
+-- Enable relative line numbers
+vim.opt_local.relativenumber = true
+
 return {}
